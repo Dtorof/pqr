599f5330534e64d6331652f2a8a503a67c085cb8
Anyelith Jacobo <anyelithj@misena.edu.co>
2022-11-22 12:35:25 -0500

    desing login, navbar,adminitration and router vue commit

--- frontend-PQR/src/components/login/Login.vue
+++ frontend-PQR/src/components/login/Login.vue
@@ -1,14 +1,41 @@
+<script setup>
+
+</script>
 <template>
-    <form>
-      <h2>Inicio de Sesión</h2>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Usuario</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
-      </div>
-      <button type="submit" class="btn btn-primary">Ingresar</button>
-    </form>
+    <div>
+      <form class="form">
+        <h2 class="form-title my-3">Inicio de Sesión</h2>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Usuario</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="submit" class="btn">Ingresar</button>
+      </form>
+    </div>
 </template>
+<style scoped>
+@import '../../assets/base.css';
+
+label {
+  color: #3f3d56;
+}
+input {
+  border: 1px solid #6c63ff;
+}
+.form {
+  margin: 20%;
+}
+.form-title {
+  color: #ccc;
+  font-weight: bolder;
+}
+.btn{
+  /* background-color: var(--color-text); */
+  background-color: #6c63ff;
+  color: white;
+}
+</style>
--- frontend-PQR/src/views/AdministrationView.vue
+++ frontend-PQR/src/views/AdministrationView.vue
@@ -3,5 +3,18 @@ import Navbar from '../components/navbar/Navbar.vue'
 
 </script>
 <template>
-    <Navbar/>
+    <div class="container-main">
+        <Navbar/>
+        <img class="img" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.slnsoftwares.com%2Fimages%2Fgif%2Fwebdevelopment10.gif&f=1&nofb=1&ipt=b13139b05e6ee422320b34cf1713cf1e31c4758181422de8a198787759858ada&ipo=images"/>
+    </div>
 </template>
+<style scoped>
+.container-main {
+    display: flex;
+    flex-direction: column;
+}
+.img {
+    width: 60%;
+    margin: 2px auto;
+}
+</style>
