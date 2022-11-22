e2e418a1eeea87d55c4e262c5255e335c352c578
yenifer ortiz herrera <yenifer.ortiz1@misena.edu.co>
2022-11-22 13:58:56 -0500

    tabla responder PQRS 1

--- frontend-PQR/src/App.vue
+++ frontend-PQR/src/App.vue
@@ -3,13 +3,14 @@ import { RouterLink, RouterView } from 'vue-router'
 import UserVue from './components/users/User.vue';
 import CustomerVue from './components/customers/Customer.vue';
 import ResponsePQRViewVue from './views/ResponsePQRView.vue';
+import AdministracionView from "./views/AdministrationView.vue"
 </script>
 
 <template>
   <!-- <UserVue/> -->
   <!-- <CustomerVue/> -->
-  <ResponsePQRViewVue/>
-  
+  <!-- <ResponsePQRViewVue/> -->
+ <!-- <AdministracionView/>  -->
   <!-- <header>
     <div class="wrapper">
       <nav> -->
